2f50f6c03a6fcbf5112db50daf87ac230dcb99e7
Tako Lansbergen <t.lansbergen@call2.nl>
2019-10-15 15:52:27 +0200

    Added docstrings

--- client/client_constants.py
+++ client/client_constants.py
@@ -1,1 +1,10 @@
-APP_NAME_VERSION = "kanbanpy v0.1"
+
+""" client_main.py: Entrypoint van de kanbanpy client applicatie
+    Auteur: Tako Lansbergen, Novi Hogeschool
+    Studentnr.: 800009968
+    Leerlijn: Python
+    Datum: 15 okt 2019
+"""
+
+APP_NAME = "kanbanpy"
+APP_VERSION = "v0.1"
--- client/client_main.py
+++ client/client_main.py
@@ -1,8 +1,19 @@
+
+""" client_main.py: Entrypoint van de kanbanpy client applicatie
+    Auteur: Tako Lansbergen, Novi Hogeschool
+    Studentnr.: 800009968
+    Leerlijn: Python
+    Datum: 15 okt 2019
+"""
+
+import os
 import client_constants
 
 # hoofdfunctie, entrypoint van de client applicatie
 def main():
-    print(client_constants.APP_NAME_VERSION)
+    
+    os.system('cls||clear')
+    print(client_constants.APP_NAME, client_constants.APP_VERSION)
 
 if __name__ == "__main__":
     main()
